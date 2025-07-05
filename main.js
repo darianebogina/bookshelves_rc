708e4a7a3ade4096f91f9689cea8a56c3695d984
Daria <dasha.nebogina@mail.ru>
2025-07-05 17:26:18 +0300

    feat: implement infinite scroll

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // https://www.googleapis.com/books/v1/volumes?q=java&filter=ebook
 let query = "javascript";
 let filter = "ebooks";
-const url = "https://www.googleapis.com/books/v1/volumes?q={query}&filter={filter}";
+const maxResults = 15;
+let startIndex = 0;
+const url = "https://www.googleapis.com/books/v1/volumes?q={query}&filter={filter}&maxResults={maxResults}&startIndex={startIndex}";
 
 const content = document.getElementsByClassName("books__cards")[0];
 const input = document.getElementsByClassName('input')[0];
@@ -9,32 +11,36 @@ const button = document.getElementsByClassName('search__button')[0];
 const select = document.getElementsByClassName('filter__books')[0];
 
 async function getBooks() {
-    let currentUrl = url.replace('{query}', query).replace('{filter}', filter);
+    let currentUrl = url.replace('{query}', query)
+        .replace('{filter}', filter)
+        .replace('{maxResults}', maxResults)
+        .replace('{startIndex}', startIndex);
+    console.log(currentUrl);
 
     let response = await fetch(currentUrl);
     let result = await response.json();
+    console.log(result);
     if (result.totalItems == 0) {
         alert("No books found");
         return;
     }
     let items = result.items;
-    console.log(items);
     let arrayOfObj = [];
 
     for (let i = 0; i < items.length; i += 1) {
+        let info = items[i].volumeInfo;
         let imgDefault = "https://placeholdit.com/290x230/dddddd/999999?text=no+photo";
         let titleDefault = "No title";
         let authorDefault = "Unknown";
         let descriptionDefault = "";
         arrayOfObj.push({
-            img: items[i].volumeInfo.imageLinks?.thumbnail 
-                    && items[i].volumeInfo.imageLinks?.smallThumbnail
-                    || imgDefault,
-            title: items[i].volumeInfo.title || titleDefault,
-            author: items[i].volumeInfo.authors.join(", ") || authorDefault,
-            description: items[i].volumeInfo.description || descriptionDefault,
+            img: info.imageLinks?.thumbnail
+                && info.imageLinks?.smallThumbnail
+                || imgDefault,
+            title: info.title || titleDefault,
+            author: info.authors ? info.authors.join(", ") : authorDefault,
+            description: info.description || descriptionDefault,
         });
-        console.log(arrayOfObj[i].title);
     }
     console.log(arrayOfObj);
     return arrayOfObj;
@@ -44,7 +50,6 @@ function createCard(arrayOfObj) {
     if (arrayOfObj == undefined) {
         return;
     }
-    content.innerHTML = "";
     for (let i = 0; i < arrayOfObj.length; i += 1) {
         let book = document.createElement("div");
         book.classList.add("book");
@@ -81,15 +86,44 @@ function queryBooks() {
     if (input.value == "") {
         alert("Please input book name");
     }
-
+    startIndex = 0;
     query = input.value;
     filter = select.value;
-    console.log(filter);
-    getBooks().then(data => createCard(data));
+
+    getBooks().then(data => {
+        content.innerHTML = "";
+        createCard(data);
+    });
 }
 
 function clearInput() {
     input.value = "";
 }
+
+function scrollInfinity() {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight - 700) {
+        throttledUpdate();
+    }
+}
+
+function throttle(func, delay) {
+    let timerFlag = 0;
+
+    return (...args) => {
+        if (timerFlag === 0) {
+            func(...args);
+            timerFlag = setTimeout(() => {
+                timerFlag = 0;
+            }, delay);
+        }
+    };
+}
+
+throttledUpdate = throttle(() => {
+    startIndex += maxResults;
+    getBooks().then(data => createCard(data));
+}, 900);
+
+window.addEventListener('scroll', scrollInfinity);
 
 getBooks().then(data => createCard(data));
